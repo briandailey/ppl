35a81206a3a14db29796cf1106c6009e4d4c238f
Matt George <mgeorge@stratasan.com>
2012-10-10 22:02:30 -0500

    gravatar filter

--- ppl/utils.py
+++ ppl/utils.py
@@ -1,6 +1,6 @@
 import re
+import hashlib
 from unicodedata import normalize
-
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
 
 def slugify(text, delim=u'-'):
@@ -11,3 +11,19 @@ def slugify(text, delim=u'-'):
         if word:
             result.append(word)
     return unicode(delim.join(result))
+
+def gravatar(email, size=100, rating='g', default='retro', force_default=False,
+             force_lower=False, use_ssl=False):
+    if use_ssl:
+        url = "https://secure.gravatar.com/avatar/"
+    else:
+        url = "http://www.gravatar.com/avatar/"
+    if force_lower:
+        email = email.lower()
+    hashemail = hashlib.md5(email).hexdigest()
+    link = "{url}{hashemail}?s={size}&d={default}&r={rating}".format(
+        url=url, hashemail=hashemail, size=size,
+        default=default, rating=rating)
+    if force_default:
+        link = link + "&f=y"
+    return link
