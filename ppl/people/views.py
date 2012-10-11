87f88687632c42121049fd047f5c2342883603e2
Matt George <mgeorge@stratasan.com>
2012-10-10 22:05:21 -0500

    adding profile instead of mock

--- ppl/people/views.py
+++ ppl/people/views.py
@@ -1,15 +1,9 @@
-import collections
 from pyramid.view import view_config
 
+from ppl.models import Profile
 
-Person = collections.namedtuple("Person", ['first', 'last', 'email'])
-@view_config(route_name="people.ext", match_param=('ext=json'), renderer="json")
 @view_config(route_name="people.list", renderer="people/list.html")
-@view_config(route_name="people.list", request_param=('format=json'), renderer="json")
-@view_config(route_name="people.list", request_param=("format=partial"), renderer="people/list_partial.html")
 def list(request):
-    people = []
-    for i in range(20):
-        people.append(Person(first="John", last="Doe%s" % i, email="jdoe#%s@acme.com" % i))
-    return {'people':people}
+    people = Profile.query.all()
+    return {'people': people}
 
