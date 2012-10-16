53154f9fabf4bd431e6cf6d360956707db86d93c
Matt George <mgeorge@stratasan.com>
2012-10-15 22:49:14 -0500

    adding group count

--- ppl/views.py
+++ ppl/views.py
@@ -1,11 +1,12 @@
 from sqlalchemy import func
 from pyramid.view import view_config
-from ppl.models import Session, Profile, Company
+from ppl.models import Session, Profile, Company, Group
 @view_config(route_name='home', renderer='index.html')
 def home(request):
     session = Session()
     people = session.query(func.count(Profile.id)).scalar()
     companies = session.query(func.count(Company.id)).scalar()
-    return {'people': people, 'companies': companies}
+    groups = session.query(func.count(Group.id)).scalar()
+    return {'people': people, 'companies': companies,'groups': groups}
 
 
