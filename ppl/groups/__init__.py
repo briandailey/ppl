f20cb9c0b5e41f169d41fffb489ea78af0e28cdd
Matt George <mgeorge@stratasan.com>
2012-10-23 21:14:53 -0500

    group list and detail page

--- ppl/groups/__init__.py
+++ ppl/groups/__init__.py
@@ -1,6 +1,16 @@
-def list(request):
-    return {}
+from ppl.models import Group
+def list_groups(request):
+    groups = Group.query.all()
+    return {'groups': groups}
+
+def detail(request):
+    slug = request.matchdict['slug']
+    group = Group.query.filter_by(slug=slug).one()
+    return {'group': group}
+
 def includeme(config):
     config.add_route('groups.list', '')
-    config.add_view(list, route_name='groups.list', renderer='groups/list.html')
+    config.add_route('groups.detail', '{slug}')
+    config.add_view(list_groups, route_name='groups.list', renderer='groups/list.html')
+    config.add_view(detail, route_name='groups.detail', renderer='groups/detail.html')
     config.scan()
