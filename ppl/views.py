3e993a3d638e7b134f6354c7b8ac17aa17f75f00
Matt George <mgeorge@gmail.com>
2013-10-25 11:18:43 -0500

    404 page

--- ppl/views.py
+++ ppl/views.py
@@ -1,6 +1,13 @@
 from sqlalchemy import func
 from pyramid.view import view_config
 from ppl.models import DBSession, Profile, Company, Group
+
+
+@view_config(context=HTTPNotFound, renderer='404.html')
+def not_found(self, request):
+    request.response.status_int = 404
+    return {}
+
 @view_config(route_name='home', renderer='index.html')
 def home(request):
     session = DBSession()
