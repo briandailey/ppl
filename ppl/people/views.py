0c36abc4236350fb88b4daa8fbc5d8831b8a19bf
Brian Dailey <brian@stratasan.com>
2013-10-30 23:01:14 -0500

    Sort people by random.

--- ppl/people/views.py
+++ ppl/people/views.py
@@ -2,11 +2,12 @@ from pyramid.view import view_config
 
 from pyramid.httpexceptions import HTTPNotFound
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import func
 from ppl.models import Profile, Tag
 
 @view_config(route_name="people.list", renderer="people/list.html")
 def list(request):
-    people = Profile.query.order_by(Profile.created_ts).all()
+    people = Profile.query.order_by(func.random()).all()
     return {'people': people}
 
 @view_config(route_name="people.detail", renderer="people/detail.html")
